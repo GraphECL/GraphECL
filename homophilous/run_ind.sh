cfa5399e2c16053982c8de40b95a5e159c302c98
GraphECL <133249560+GraphECL@users.noreply.github.com>
2023-09-30 21:22:58 -0400

    Update run_ind.sh

--- homophilous/run_ind.sh
+++ homophilous/run_ind.sh
@@ -1,9 +1,3 @@
-
-python main.py --dataname cora --epochs 100 --lr1 5e-4 --lr2 1e-2 --wd1 1e-6 --wd2 1e-5 --n_layers 1 --hid_dim 1024  --temp 0.5  --num_MLP 1 
-python main.py --dataname citeseer --epochs 100 --lr1 1e-3 --lr2 1e-2 --wd1 1e-5 --wd2 1e-2  --n_layers 2 --hid_dim 2048  --temp 0.5 --num_MLP 1 
-python main.py --dataname pubmed --epochs 100 --lr1 5e-4 --lr2 1e-2 --wd1 0 --wd2 1e-4 --n_layers 2 --hid_dim 1024  --temp 0.5 --num_MLP 1
-python main.py --dataname comp --epochs 700 --lr1 5e-4 --lr2 1e-2 --wd1 0 --wd2 5e-4 --n_layers 1 --hid_dim 2048  --temp 0.99 --moving_average_decay 0.999 --num_MLP 0
-python main.py --dataname photo --epochs 200 --lr1 5e-4 --lr2 1e-2 --wd1 0 --wd2 1e-3 --n_layers 1 --hid_dim 2048  --temp 0.99 --moving_average_decay 0.9 --num_MLP 0
-
-
-
+python main_ind.py --dataname citeseer --epoch 100 --lr1 0.0005 --lambda_loss 1 --moving_average_decay 0.0 --hid_dim 1024 --temp 0.9 --lam 0.01 --num_MLP 0 --n_layers 1 --wd2 0.01
+python main_ind.py --dataname pubmed --epoch 500 --lr1 0.0005 --lambda_loss 1 --moving_average_decay 0.0 --hid_dim 4096 --temp 0.7 --lam 0.5 --num_MLP 2 --n_layers 1 --wd2 0.01
+python main_ind.py --dataname photo --epoch 700 --lr1 0.0005 --lambda_loss 1 --moving_average_decay 0.0 --hid_dim 2048 --temp 0.5 --lam 0.01 --num_MLP 1 --n_layers 2
